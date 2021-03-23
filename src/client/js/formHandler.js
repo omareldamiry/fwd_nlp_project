59f2cd63b8894a8aaebdd78b1cdf419b610a7a6a
Omareldamiry <omar.eldamiry@gmail.com>
2021-03-23 22:37:55 +0300

    Added formHandler and nameChecker

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,41 @@
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let formText = document.getElementById('name').value;
+    Client.checkForName(formText);
+
+    console.log("::: Form Submitted :::");
+    
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // });
+
+    postData('http://localhost:8081/add-url', { url: formText })
+    .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+    });
+}
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+export { handleSubmit }
